0cfee92e1558dcfe1a73e072871d0c31f85f3193
adryansyah29 <116249899+adryansyah29@users.noreply.github.com>
2024-01-15 07:20:34 +0700

    Delete Dashboard directory

--- Dashboard/Dashboard.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import streamlit as st
-from babel.numbers import format_currency
-sns.set(style='dark')
-
-sewasepedajam_df=pd.read_csv("dashboard/all_data.csv")
-sewasepedajam_df.head()
